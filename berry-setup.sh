19898fc2513b1217f0517768b659c16c9d3aaf98
McNichol <mcnichol.m@gmail.com>
2016-12-29 05:44:12 +0000

    Add gitignore, ZSH env setup

--- berry-setup.sh
+++ berry-setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+
+#Ask user for SUDO Pass
+read -p "Enter SUDO Password: " -s pass
+
+echo $pass
+
+sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 
+#Setup Locales and make Default Locale EN_US
 locale_pkg=$(dpkg -l locales | grep -i locales)
 installed=$(echo $locale_pkg | awk '{print $1}')
 package_name=$(echo $locale_pkg | awk '{print $2}')
@@ -13,6 +20,11 @@ else
     echo "Locales Not Installed. Installing"
     aptitude install locales
 fi
+
+#ZSH Setup and oh-my-zsh
+echo $pass | sudo -S apt-get -y install zsh
+echo $pass | sudo -S chsh -s $(which zsh)
+touch ~/.zshrc
 
 sudo apt-get -y install vim
 sudo apt-get -y install tmux
